527a43af3076da74abf61b5fd4d955f8e7804c09
Anthony Wiryaman <anthonywiryaman@gmail.com>
2018-11-15 18:55:28 -0500

    Update JotTableViewCell.swift

--- Contuity/Views/Explore/JotTableViewCell/JotTableViewCell.swift
+++ Contuity/Views/Explore/JotTableViewCell/JotTableViewCell.swift
@@ -14,13 +14,9 @@ class JotTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
-    
 }
